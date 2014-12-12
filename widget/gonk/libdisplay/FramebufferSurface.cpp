d2757376b045f63303b6a7e87c1d5c849b4bd1ac
Yunchao Chen <chenyc@infthink.com>
2014-12-12 15:15:50 +0800

    Disable unnecessary Logs.
    Signed-off-by: Yunchao Chen <chenyc@infthink.com>

--- widget/gonk/libdisplay/FramebufferSurface.cpp
+++ widget/gonk/libdisplay/FramebufferSurface.cpp
@@ -171,7 +171,7 @@ int FramebufferSurface::GetPrevFBAcquireFd() {
     if (mPrevFBAcquireFence.get() && mPrevFBAcquireFence->isValid()) {
         return mPrevFBAcquireFence->dup();
     }
-    ALOGE("GetPrevFBAcquireFd: FB acquire fence is invalid!");
+    //ALOGE("GetPrevFBAcquireFd: FB acquire fence is invalid!");
     return -1;
 }
 
