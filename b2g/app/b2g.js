4024ac4652a9f683a0016b9ce47ea0fdadb6959a
Wu Dongyang <wudongyang@infthink.com>
2014-12-23 09:48:38 +0800

    SNTP refresh every 3 hours.

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -894,7 +894,7 @@ pref("disk_space_watcher.enabled", true);
 
 // SNTP preferences.
 pref("network.sntp.maxRetryCount", 10);
-pref("network.sntp.refreshPeriod", 86400); // In seconds.
+pref("network.sntp.refreshPeriod", 10800); // In seconds.
 pref("network.sntp.pools", // Servers separated by ';'.
      "133.100.11.8;0.pool.ntp.org;1.pool.ntp.org;2.pool.ntp.org;3.pool.ntp.org");
 pref("network.sntp.port", 123);
